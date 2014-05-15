f4c79f4de4612407d2df057c87b03cef24a46b72
sim.romano@gmail.com <sim.romano@gmail.com>
2014-05-15 15:44:43 +0200

    knockout

---
+++ StallmateDemo/scripts/app.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+	ko.applyBindings({
+		stalls: [
+				{ name: 'Stallo 1', address: 'Lorem ipsum dolor sit amet' },
+				{ name: 'Stallo 2', address: 'consectetur adipisici elit' },
+				{ name: 'Stallo 3', address: 'sed eiusmod tempor incidunt ut labore et dolore magna aliqua' },
+				{ name: 'Stallo 4', address: ' Ut enim ad minim veniam, quis nostrud' },
+				{ name: 'Stallo 5', address: 'exercitation ullamco laboris nisi ut aliquid' },
+				{ name: 'Stallo 6', address: 'ex ea commodi consequat' },
+				{ name: 'Stallo 7', address: 'Quis aute iure reprehenderit in voluptate' },
+				{ name: 'Stallo 8', address: 'velit esse cillum dolore eu fugiat nulla pariatur' },
+				{ name: 'Stallo 9', address: 'Excepteur sint obcaecat cupiditat non proident' }
+		]
+	});
+
+});
+
+
